673330b31c2cc9684c2cb8cfce69563e77098038
doubletcjs <doubletcjs@gmail.com>
2020-05-10 01:34:44 +0800

    update

--- Electron/demo/electrondemo03/src/index.js
+++ Electron/demo/electrondemo03/src/index.js
@@ -1,7 +1,4 @@
-import {
-  app,
-  BrowserWindow
-} from 'electron';
+import { app, BrowserWindow } from 'electron';
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
@@ -17,9 +14,6 @@ const createWindow = () => {
   mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
-    webPreferences: {
-      nodeIntegration: true
-    }
   });
 
   // and load the index.html of the app.
