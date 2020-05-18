1b3153a047ef74e3d2cbc942f0a30b127a2d34f3
doubletcjs <doubletcjs@gmail.com>
2020-05-18 23:04:11 +0800

    update

--- Flutter/hksy_flutter/lib/pages/mine/views/minesectionview.dart
+++ Flutter/hksy_flutter/lib/pages/mine/views/minesectionview.dart
@@ -26,48 +26,58 @@ class _MineSectionViewState extends State<MineSectionView> {
             String additional = data["additional"];
             int index = this.widget.dataSources.indexOf(data);
             return Container(
-              padding: EdgeInsets.fromLTRB(20, 18.5, 20, 18.5),
-              child: Row(
+              padding: EdgeInsets.fromLTRB(20, 18.5, 20, 0),
+              child: Column(
                 children: <Widget>[
-                  Image.asset(
-                    icon,
-                    width: 23,
-                    fit: BoxFit.fitWidth,
-                  ),
-                  SizedBox(
-                    width: 13.5,
-                  ),
-                  Text(
-                    name,
-                    style: TextStyle(
-                      color: rgba(255, 255, 255, 1),
-                      fontSize: 15,
-                    ),
-                  ),
-                  Expanded(
-                    child: Container(),
-                  ),
                   Row(
                     children: <Widget>[
-                      additional == null
-                          ? Container()
-                          : Text(
-                              additional,
-                              style: TextStyle(
-                                color: rgba(145, 152, 173, 1),
-                                fontSize: 13,
-                              ),
-                            ),
-                      SizedBox(
-                        width: additional == null ? 0 : 10,
-                      ),
                       Image.asset(
-                        "images/right_arrow@3x.png",
-                        width: 20,
+                        icon,
+                        width: 23,
                         fit: BoxFit.fitWidth,
+                      ),
+                      SizedBox(
+                        width: 13.5,
+                      ),
+                      Text(
+                        name,
+                        style: TextStyle(
+                          color: rgba(255, 255, 255, 1),
+                          fontSize: 15,
+                        ),
+                      ),
+                      Expanded(
+                        child: Container(),
+                      ),
+                      Row(
+                        children: <Widget>[
+                          additional == null
+                              ? Container()
+                              : Text(
+                                  additional,
+                                  style: TextStyle(
+                                    color: rgba(145, 152, 173, 1),
+                                    fontSize: 13,
+                                  ),
+                                ),
+                          SizedBox(
+                            width: additional == null ? 0 : 8,
+                          ),
+                          Image.asset(
+                            "images/right_arrow@3x.png",
+                            width: 20,
+                            fit: BoxFit.fitWidth,
+                          ),
+                        ],
                       ),
                     ],
                   ),
+                  Container(
+                    margin: EdgeInsets.fromLTRB(0, 18.5, 0, 0),
+                    height:
+                        index < this.widget.dataSources.length - 1 ? 0.8 : 0,
+                    color: rgba(21, 25, 54, 1),
+                  )
                 ],
               ),
             );
