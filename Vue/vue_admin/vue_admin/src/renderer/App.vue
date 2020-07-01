4119a072633e0a8d49c51b1db1e769c397b9cf9d
doubletcjs <doubletcjs@gmail.com>
2020-07-01 18:13:18 +0800

    update

--- Vue/vue_admin/vue_admin/src/renderer/App.vue
+++ Vue/vue_admin/vue_admin/src/renderer/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <router-link to="/">联系我们</router-link>
+    <router-link to="/">主页</router-link>
     <router-link to="/about">关于</router-link>
     <router-view></router-view>
   </div>
